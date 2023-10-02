61b09b4983ba1502882bd6cbca738e30b305be20
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-10-02 16:38:13 +0200

    Query werkt #6

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,14 +2,34 @@ import { gql } from 'graphql-request'
 import { hygraph } from '$lib/utils/hygraph.js'
 
 export async function load() {
+  
   let query = gql`
-    query Quotes {
-      quotes(first: 10) {
-        author
-        text
+  query Assets {
+    dashboard(where: {id: "cln32p50anrma0bw03q8fsa54"}) {
+      cirkelDiagramContinents {
+        cirkelDiagramNumberOcean
       }
+      cirkelDiagramRiverOcean {
+        cirkelDiagramNumberOcean
+      }
+      infotext {
+        infotext {
+          html
+          text
+        }
+        id
+      }
+      trashRemoved {
+        trashRemovedKg
+        trashRemovedText {
+          html
+          text
+        }
+      }
+      title
     }
+  }
+  
   `
-
   return await hygraph.request(query)
 }
