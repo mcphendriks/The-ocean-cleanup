27fc4ff3e48e3ed6ef864d773420cebecf07b02d
TessaViergever <112861180+TessaViergever@users.noreply.github.com>
2023-10-04 16:11:49 +0200

    query aanpassen

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -6,25 +6,12 @@ export async function load() {
   let query = gql`
   query Assets {
     dashboard(where: {id: "cln32p50anrma0bw03q8fsa54"}) {
-      cirkelDiagramContinents {
-        cirkelDiagramNumberOcean
-      }
-      cirkelDiagramRiverOcean {
-        cirkelDiagramNumberOcean
-      }
       infotext {
         infotext {
           html
           text
         }
         id
-      }
-      trashRemoved {
-        trashRemovedKg
-        trashRemovedText {
-          html
-          text
-        }
       }
       title
     }
