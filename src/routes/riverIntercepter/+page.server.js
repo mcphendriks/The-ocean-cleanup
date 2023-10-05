62a1ded609d97dd58937b52dd6c6eacdb62dc48e
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-10-05 19:29:10 +0200

    opzet

---
+++ src/routes/riverIntercepter/+page.server.js
@@ -0,0 +1,41 @@
+import { gql } from 'graphql-request'
+import { hygraph } from '$lib/utils/hygraph.js'
+import { json } from '@sveltejs/kit'
+
+export async function load() {
+  
+  let query = gql`
+  query Assets {
+    dashboard(where: {id: "cln32p50anrma0bw03q8fsa54"}) {
+      infotext {
+        infotext {
+          html
+          text
+        }
+        id
+      }
+      trashRemoved {
+        trashRemovedText {
+          html
+          text
+        }
+      }
+      title
+      river {
+        riverArea {
+          text
+        }
+        riverImage {
+          url
+        }
+      }
+    }
+  }  
+  `
+  const dataHygraph = await hygraph.request(query)
+  
+  const grrrData = await fetch("https://fdnd-toc-api.netlify.app/river")
+        const dataApi = await grrrData.json()
+  return {dataHygraph, dataApi}
+}
+
