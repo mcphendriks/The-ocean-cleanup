5e6d40d78cbd256c3e91240c86b6fdbc8e2b4f45
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-10-09 21:19:00 +0200

    Add map component + Multiple endpoints #8 #6

--- src/components/map.svelte
+++ src/components/map.svelte
@@ -1,20 +1,53 @@
-<!-- <script>
-	// Import necessary modules
+<script>
 	import { onMount } from 'svelte';
 
-	// Define Mapbox access token
-	mapboxgl.accessToken =
-		'pk.eyJ1IjoibWNwaGVuZHJpa3MiLCJhIjoiY2xuYWpkajM3MDRvNzJxbzdjbGg1YXc0MiJ9.mRRivdosZVdSXQ9FDd0ZwA';
-
-	let map; // Declare map variable outside of onMount
+	let map;
 
 	onMount(() => {
-		// Create a map instance
+		mapboxgl.accessToken =
+			'pk.eyJ1IjoibWNwaGVuZHJpa3MiLCJhIjoiY2xuYWpkajM3MDRvNzJxbzdjbGg1YXc0MiJ9.mRRivdosZVdSXQ9FDd0ZwA';
+
 		map = new mapboxgl.Map({
 			container: 'map',
 			style: 'mapbox://styles/mapbox/light-v11',
-			zoom: 12,
-			center: [-122.4473, 37.7535]
+			zoom: 2,
+			center: [-103.5917, 40.6699]
+		});
+
+		map.on('load', () => {
+			map.addSource('interceptors', {
+				type: 'geojson',
+				data: 'https://raw.githubusercontent.com/mcphendriks/the-ocean-clean-up-GeoJSON-data/main/data.geojson',
+				cluster: true,
+				clusterMaxZoom: 14,
+				clusterRadius: 50
+			});
+
+			map.addLayer({
+				id: 'interceptor-points',
+				type: 'circle',
+				source: 'interceptors',
+				paint: {
+					'circle-radius': 10,
+					'circle-stroke-width': 1,
+					'circle-stroke-color': '#fff',
+					'circle-color': [
+						'match',
+						['get', 'status'],
+						'in_operation',
+						'#84CE5F',
+						'in_maintenance',
+						'#143653',
+						'installed_for_testing',
+						'#6CCDE1',
+						'contract_signed',
+						'#B0B0B0',
+						'planned',
+						'#B0B0B0',
+						/* Default color if no match found */ 'red'
+					]
+				}
+			});
 		});
 	});
 </script>
@@ -25,6 +58,6 @@
 	#map {
 		position: relative;
 		width: 100%;
-		height: 500px; /* Set the map height as per your preference */
+		height: 500px;
 	}
-</style> -->
+</style>
